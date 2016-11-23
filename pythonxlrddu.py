0d18d76592411d4bea521acad01f22bf33e31436
wHy-Xs <603987690@qq.com>
2016-11-23 15:40:07 +0800

    Update pythonxlrddu.py

--- pythonxlrddu.py
+++ pythonxlrddu.py
@@ -24,3 +24,61 @@ print sheet1.row_values(0)
 #ctype : 0 empty,1 string, 2 number, 3 date, 4 boolean, 5 error
 #col_values(0)获取整列数
 #如果遇到中文可以现获取这个列的值，然后再unicode，如果想直接输出中文，则需要先判断类型然后再看是否需要encode('utf-8')
+#################################################################################
+#################################################################################
+import xlrd
+
+class readExcel():
+    def __init__(self):
+        workbook = xlrd.open_workbook(r'D:\pythonCode/text.xlsx')  #加载文件
+        self.workbook= workbook
+        sheetNames=workbook.sheet_names()   #查看这个文件中有多少表，并把它们名字拿出来
+        print sheetNames  #打印表的名称
+
+    #选择哪张表
+    def selectSheet(self,n):
+        if n == 0:
+            sheet1=self.workbook.sheet_names()[0]
+            onlysheet = sheet1
+        else:
+            sheet=self.workbook.sheet_names()[n]
+            onlysheet = sheet
+        return onlysheet
+
+    def readContext(self,onlysheet,row,col):
+        sheetContext=self.workbook.sheet_by_name(onlysheet)
+        print '这是表'+str(onlysheet)+".本表有"+str(sheetContext.nrows)+"行"+","+str(sheetContext.ncols)+"列."
+        print sheetContext
+
+        title=sheetContext.row_values(0)#取得本表的标题
+        for i in title:
+            print unicode(i),   #输出本表的标题
+        print ''
+
+        if row>sheetContext.nrows or col>sheetContext.ncols:
+            return False
+        else:
+            context=sheetContext.row(row)[col].value
+            print context
+        
+    def printdd(selfmai):
+        print 'dfdf'
+
+
+def main():
+    excel=readExcel()
+    excel.printdd()
+    print("请选择查看哪张表:")
+    sheetindex=input()
+    onlysheet=excel.selectSheet(sheetindex)
+    print '请选择查看具体位置的信息:'
+    local1=input()
+    local2=input()
+    excel.readContext(onlysheet,local1,local2)
+
+if __name__ == "__main__":
+     main()
+
+    
+
+
