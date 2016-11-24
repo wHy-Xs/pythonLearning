f143de38390195f3dd547c5a21e70497859f279d
wHy-Xs <603987690@qq.com>
2016-11-24 16:15:57 +0800

    Update pythonSortAndSearch.py

--- pythonSortAndSearch.py
+++ pythonSortAndSearch.py
@@ -35,6 +35,28 @@ arr=[46,243,1,3,6,8,9]
 k=bubbleSort(arr)
 print k
 ####################################
+########插入排序#####################
+def insert_sort(lists):
+    # 插入排序
+    count = len(lists)
+    for i in range(1, count):
+        key = lists[i]#要插入的数据
+        j = i - 1    #已经排好的序列
+        while j >= 0:
+            if lists[j] > key: #从后往前比较，找到合适的位置插入
+                lists[j + 1] = lists[j] 
+                lists[j] = key
+            j -= 1
+    return lists
+######################################
+'''
+1. 从第1个元素开始，该元素可认为已排序
+2. 取下一个元素，对已排序数组从后往前扫描
+3. 若从排序数组中取出的元素大于新元素，则移至下一位置
+4. 重复步骤3，直到找到已排序元素小于或等于新元素的位置
+5. 插入新元素至该位置
+6. 重复2~5
+'''
 ######二分法查找######################
 def binarySearch(self, nums, target):
         # write your code here
