dc2e2b33573cdc2103cb1d4be91212ca3c8e388e
wHy-Xs <603987690@qq.com>
2016-11-24 17:28:05 +0800

    Update pythonSortAndSearch.py

--- pythonSortAndSearch.py
+++ pythonSortAndSearch.py
@@ -57,6 +57,52 @@ def insert_sort(lists):
 5. 插入新元素至该位置
 6. 重复2~5
 '''
+#####################################
+############归并排序##################
+def merge_sort(aList):  
+    n = len(aList)  
+    # 只有一个元素，“触底”了，直接返回  
+    if n <= 1:  
+        return aList  
+                # 分割成两部分  
+    mid = n // 2
+    
+    left = merge_sort(aList[:mid])  
+    right = merge_sort(aList[mid:])
+    print left
+    print 'ssssssssssssssssssss'
+    print right
+    print 'dddddddddddddddddddd'
+    # 合并  
+    return merge_sort_helper(left, right)  
+              
+              
+# 这个函数用来合并两个排好序的数组  
+def merge_sort_helper(l1, l2):  
+    result = []  
+    index1 = index2 = 0  
+    n1 = len(l1)  
+    n2 = len(l2)  
+    while index1 != n1 and index2 != n2:  
+        if l1[index1] < l2[index2]:  
+            result.append(l1[index1])  
+            index1 += 1  
+        else:  
+            result.append(l2[index2])  
+            index2 += 1  
+    result.extend(l2[index2:])  
+    result.extend(l1[index1:])  
+    return result  
+
+
+a=[10,4,6,3,8,2,5,7]
+k=merge_sort(a)
+print k
+#####################################
+'''
+归并就是先将数组分成两部分直至最简，然后用另一个函数处理最简得到结果返回，依次这样处理，最终的结果就是我们要得到的结果。
+详细讲解：http://blog.csdn.net/guoziqing506/article/details/50949854
+'''
 ######二分法查找######################
 def binarySearch(self, nums, target):
         # write your code here
