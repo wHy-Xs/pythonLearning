aada11b72dc3674ae1d2f39e84292ca31c5a95c8
wHy-Xs <603987690@qq.com>
2017-03-17 16:17:54 +0800

    Update python栈的实现.py

--- python栈的实现.py
+++ python栈的实现.py
@@ -32,3 +32,38 @@ s.push(8.4)
 print(s.pop()) 
 print(s.pop()) 
 print(s.size()) 
+
+
+
+###########################################################################
+队列
+class queue():  
+    def __init__(self):  
+        self.queue = []  
+    def empty(self):  
+        return self.queue == []  
+    def enqueue(self,data):  
+        self.queue.append(data)  
+    def dequeue(self):  
+        if self.empty():  
+            return None  
+        else:  
+            return self.queue.pop(0)  
+    def head(self):  
+        if self.empty():  
+            return None  
+        else:  
+            return self.queue[0]  
+    def length(self):  
+        return len(self.queue)  
+
+if __name__ == '__main__':
+    a=queue()
+    a.enqueue(3)
+    a.enqueue(4)
+    a.enqueue(5)
+    print a.length()
+    print a.dequeue()
+    print a.dequeue()
+    print a.dequeue()
+    
