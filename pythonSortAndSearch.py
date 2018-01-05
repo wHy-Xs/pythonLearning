33832fb59f91ee86f4ce014f2da8928f5cc9d7fa
wHy-Xs <603987690@qq.com>
2018-01-05 15:57:32 +0800

    Update pythonSortAndSearch.py

--- pythonSortAndSearch.py
+++ pythonSortAndSearch.py
@@ -289,3 +289,30 @@ public class QuickSort {
         }
     }
 }
+#################################################################
+#希尔排序
+ public static void shellSort(int[] data) {  
+        // 计算出最大的h值  
+        int h = 1;  
+        while (h <= data.length / 3) {  
+            h = h * 3 + 1;  
+        }  
+        while (h > 0) {  
+            for (int i = h; i < data.length; i += h) {  
+                if (data[i] < data[i - h]) {  
+                    int tmp = data[i];  
+                    int j = i - h;  
+                    while (j >= 0 && data[j] > tmp) {  
+                        data[j + h] = data[j];  
+                        j -= h;  
+                    }  
+                    data[j + h] = tmp;  
+                    print(data);  
+                }  
+            }  
+            // 计算出下一个h值  
+            h = (h - 1) / 3;  
+        }  
+    }  
+ ##################################################
+思想：1.选择一个小的间隔h,比较第一个位置和第h个位置的大小，并且互换。
